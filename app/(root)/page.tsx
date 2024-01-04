c38fca39c69d080b1cc32f347e1e58e1f34ba6b7
Nojus Stankevicius <nojus@happyweb.dk>
2024-01-04 19:13:58 +0200

    adding clerk authentification

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
+export default function Home() {
+    return (
+     <div className="p-5">
+                <Button size="" variant="outline">Click me</Button>
+
+        <Card></Card>
+     </div>
+    )
+}
